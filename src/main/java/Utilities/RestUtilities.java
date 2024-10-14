a00dbf7a490a61d32303a10e2102d40978c389c0
PhyoThihaOo <phyothihaoo.pto@gmail.com>
2024-10-14 14:18:38 -0400

    final commit

--- src/main/java/Utilities/RestUtilities.java
+++ src/main/java/Utilities/RestUtilities.java
@@ -1,2 +1,31 @@
-package Utilities;public class RestUtilities {
+package Utilities;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class RestUtilities {
+
+    public static Response performPost(String endpoint, String requestPayload, Map<String,String> headers){
+
+        return given().
+                baseUri(endpoint).
+                contentType(ContentType.JSON).
+                body(requestPayload).
+                headers(headers).
+                when().
+                post().
+                then().
+                log().
+                all().
+                extract().response();
+
+
+
+    }
+
+
 }
